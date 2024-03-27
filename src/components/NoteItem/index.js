7fc6b072f41d602cf74412757cf54f8fc0ba8849
Abhinay Ponaganti <ponagantiabhinay@gmail.com>
2024-03-27 13:08:49 +0000

    Notes App

---
+++ src/components/NoteItem/index.js
@@ -0,0 +1,14 @@
+import {ItemContainer, ItemTitle, ItemNote} from './styledComponents'
+
+const NoteItem = props => {
+  const {note} = props
+
+  return (
+    <ItemContainer>
+      <ItemTitle>{note.title}</ItemTitle>
+      <ItemNote>{note.note}</ItemNote>
+    </ItemContainer>
+  )
+}
+
+export default NoteItem
